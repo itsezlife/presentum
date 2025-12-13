8d0021c6409c8b3bd7a687f0810c5776d84de483
Emil <emilzulufov.commercial@gmail.com>
2025-12-13 22:12:44 +0500

    chore: moved default event handler impl

--- lib/src/controller/event_handler.dart
+++ lib/src/controller/event_handler.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:presentum/src/controller/storage.dart';
 import 'package:presentum/src/state/payload.dart';
 import 'package:presentum/src/state/state.dart';
 
@@ -130,4 +131,48 @@ abstract interface class IPresentumEventHandler<
 
   /// Called when any presentation event occurs
   FutureOr<void> onEvent(PresentumEvent<TResolved, S, V> event);
+}
+
+/// {@template presentum_storage_event_handler}
+/// Event handler for the presentum storage.
+/// {@endtemplate}
+final class PresentumStorageEventHandler<
+  TResolved extends ResolvedPresentumVariant<PresentumPayload<S, V>, S, V>,
+  S extends PresentumSurface,
+  V extends PresentumVisualVariant
+>
+    implements IPresentumEventHandler<TResolved, S, V> {
+  /// {@macro presentum_storage_event_handler}
+  const PresentumStorageEventHandler({required this.storage});
+
+  /// The storage used by to store data about dismissals/conversions/shown
+  /// items.
+  final PresentumStorage<S, V> storage;
+
+  @override
+  FutureOr<void> onEvent(PresentumEvent<TResolved, S, V> event) async {
+    switch (event) {
+      case PresentumShownEvent(:final item, :final timestamp):
+        await storage.recordShown(
+          item.id,
+          at: timestamp,
+          surface: item.surface,
+          variant: item.visualVariant,
+        );
+      case PresentumDismissedEvent(:final item, :final timestamp):
+        await storage.recordDismissed(
+          item.id,
+          at: timestamp,
+          surface: item.surface,
+          variant: item.visualVariant,
+        );
+      case PresentumConvertedEvent(:final item, :final timestamp):
+        await storage.recordConverted(
+          item.id,
+          at: timestamp,
+          surface: item.surface,
+          variant: item.visualVariant,
+        );
+    }
+  }
 }
