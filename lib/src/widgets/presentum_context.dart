c5758aa797cea20253a205132ff82cfe1a294a43
Emil <emilzulufov.commercial@gmail.com>
2025-12-14 21:38:39 +0500

    docs: introduced better docs for InheritedPresentumResolvedVariant

--- lib/src/widgets/presentum_context.dart
+++ lib/src/widgets/presentum_context.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/material.dart';
 import 'package:presentum/src/state/payload.dart';
 import 'package:presentum/src/state/state.dart';
@@ -22,23 +24,49 @@ class InheritedPresentumResolvedVariant<
   /// The item that is being presented.
   final TResolved item;
 
-  /// Get the [InheritedPresentumResolvedVariant] instance from the context.
+  /// Method that allows widgets to access a [ResolvedPresentumVariant] instance
+  /// as long as their `BuildContext` contains an
+  /// [InheritedPresentumResolvedVariant] instance.
+  ///
+  /// If we want to access an instance of a resolved presentum variant which was
+  /// provided higher up in the widget tree we can do so via:
+  ///
+  /// ```dart
+  /// InheritedPresentumResolvedVariant.of<MyResolvedVariant, MySurface, MyVisualVariant>(context);
+  /// ```
   static InheritedPresentumResolvedVariant<TResolved, S, V> of<
     TResolved extends ResolvedPresentumVariant<PresentumPayload<S, V>, S, V>,
     S extends PresentumSurface,
     V extends PresentumVisualVariant
   >(BuildContext context, {bool listen = false}) =>
       maybeOf<TResolved, S, V>(context, listen: listen) ??
-      _notFoundInheritedWidgetOfExactType();
+      _notFoundInheritedWidgetOfExactType<TResolved, S, V>();
 
-  static Never _notFoundInheritedWidgetOfExactType() => throw ArgumentError(
-    'Out of scope, not found inherited widget '
-        'a InheritedPresentationItem of the exact type',
-    'out_of_scope',
-  );
+  static Never _notFoundInheritedWidgetOfExactType<TResolved, S, V>() =>
+      throw FlutterError('''
+      InheritedPresentumResolvedVariant.of<$TResolved, $S, $V>() called with a context that does 
+      not contain a resolved presentum variant.
+      No ancestor could be found starting from the context that was passed to 
+      InheritedPresentumResolvedVariant.of<$TResolved, $S, $V>().
 
-  /// Get the [InheritedPresentumResolvedVariant] instance from the context, if
-  /// it exists.
+      This can happen if the context you used comes from a widget above the InheritedPresentumResolvedVariant<$TResolved, $S, $V>().
+
+      To fix this:
+      1. Make sure your widget is wrapped with an InheritedPresentumResolvedVariant<$TResolved, $S, $V>()
+      2. Ensure the generic types match exactly between the provider and consumer
+      3. Check that the context comes from a widget below the InheritedPresentumResolvedVariant<$TResolved, $S, $V>() in the widget tree
+    ''');
+
+  /// Method that allows widgets to access a [ResolvedPresentumVariant] instance
+  /// as long as their `BuildContext` contains an
+  /// [InheritedPresentumResolvedVariant] instance.
+  ///
+  /// If we want to access an instance of a resolved presentum variant which was
+  /// provided higher up in the widget tree we can do so via:
+  ///
+  /// ```dart
+  /// InheritedPresentumResolvedVariant.of<MyResolvedVariant, MySurface, MyVisualVariant>(context);
+  /// ```
   static InheritedPresentumResolvedVariant<TResolved, S, V>? maybeOf<
     TResolved extends ResolvedPresentumVariant<PresentumPayload<S, V>, S, V>,
     S extends PresentumSurface,
